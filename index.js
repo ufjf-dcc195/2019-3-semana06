e191ff03f356cd166313a7ae3e5b0a2d1c72d4aa
Igor Knop <igorknop@gmail.com>
2019-09-24 22:00:00 -0300

    adiciona rotas

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.use("/", function (req, res) {
-    res.json({mensagem:"Hello world!"});
-});
+app.use("/", pedeNome);
+app.use("/", saudacao);
+
 app.listen(8080);
+
+function pedeNome(req, res, next) {
+    if(req.query.nome){
+        next();
+    }else{
+        res.send("Qual seu nome?");
+    }
+}
+
+function saudacao(req, res) {
+    res.send(`Olá ${req.query.nome}!`);
+}
+function hello(req, res) {
+    res.send({mensagem:"Hello world!"});
+}
