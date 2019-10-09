7e2eb7b51d83b979b965c019847ee41c85f98bb8
Igor Knop <igorknop@gmail.com>
2019-10-08 21:55:50 -0300

    Adiciona novos módulos em modo de prod e dev

--- config/express.js
+++ config/express.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const morgan = require("morgan");
+const compression = require("compression");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 module.exports = function(){
     const app = express();
+    if(process.env.NODE_ENV === "devel"){
+        app.use(morgan('dev'));
+    } else {
+        app.use(compression());
+    }
+    app.use(bodyParser.urlencoded({"extended": true}));
+    app.use(bodyParser.json());
+    app.use(methodOverride());
     require("../app/routes/home.js")(app);
     return app;
 }
